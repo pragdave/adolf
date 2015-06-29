3ec666fa39773db599ffade3953ffb4f0c869375
pragdave <dave@pragprog.org>
2015-06-29 16:42:44 -0500

    initial load

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my wonder project
---
+++ cc.py
@@ -0,0 +1,29 @@
+progress = 0
+cont = 1
+multiplier = 1
+clickmulti = 1
+
+
+print("PRESS ENTER FOR PROGRESS OR SOMETHING!")
+
+def theOnlyfunction():
+    global progress
+    global multiplier
+    while progress < (50*clickmulti):
+        doit = input()
+        if doit == (''):
+            progress += multiplier
+            print (progress)
+def Ilied():
+    global multiplier
+    global clickmulti
+    global progress
+    global multiplier
+    print('')
+    print ('CONGRATS! YOU GOT TO',(progress),'AND NOW YOU GET',(multiplier*2),'PER ENTER!')
+    multiplier *= 2
+    clickmulti *= 2.5
+
+while cont == 1:
+    theOnlyfunction()
+    Ilied()
